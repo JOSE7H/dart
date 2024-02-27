ef7b948898c8f0c3cf2f92527677477650684f20
joseph Munyao <josephmunyao019@gmail.com>
2024-02-27 04:14:01 -0800

    first commit

---
+++ project.dart
@@ -0,0 +1,15 @@
+void main() {
+  // declear variables
+  String name = 'dart class';
+  int age = 76;
+  double price = 100.98;
+  var year = 2024;
+  num students = 460;
+
+  print(name);
+  print('dart class');
+  print(age);
+  print(price);
+  print(year);
+  print(students);
+}
